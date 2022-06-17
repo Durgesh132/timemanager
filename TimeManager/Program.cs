be3868232cdd241f89be0d5bd0f1484d3549e5de
DURGESH KUMAR <durgeshjnv2010@hotmail.com>
2022-06-17 19:33:00 +0530

    time manager

--- TimeManager/Program.cs
+++ TimeManager/Program.cs
@@ -4,9 +4,24 @@ namespace TimeManager
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("WelCome to Time Manager \n");
+
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromSeconds(5);
+
+            WriteLog writeLog = new WriteLog();
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+                writeLog.DoSomething();  //Call any function
+            }, null, startTimeSpan, periodTimeSpan);
+
+
+            Console.ReadLine();
         }
     }
 }
---
+++ TimeManager/WriteLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeManager
+{
+    public class WriteLog
+    {
+        private readonly TimeManager _timer;
+        public WriteLog()
+        {
+            //_timer = new TimeManager();
+
+            //if (!_timer.IsTimerStarted)
+            //{
+            //    _timer.PrepareTimer(() => DoSomething());
+            //}
+
+        }
+
+
+        public Action DoSomething()
+        {
+            Console.WriteLine("Hello !!");
+            return null;
+        }
+    }
+}
